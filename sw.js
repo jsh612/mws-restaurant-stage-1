90cd3b42159c6eb29119cb06f2c5b6a28229a53a
seungho Jeon <urijsh612@gmail.com>
2019-09-22 21:39:02 +0900

    offline settiing done

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('restaurant')
+    caches.open('restaurants')
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(
@@ -12,22 +12,12 @@ self.addEventListener('install', function(event) {
               '/js/dbhelper.js',
               '/data/restaurants.json',
               '/js/restaurant_info.js',
-              '/img/1.jpg',
-              '/img/2.jpg',
-              '/img/3.jpg',
-              '/img/4.jpg',
-              '/img/5.jpg',
-              '/img/6.jpg',
-              '/img/7.jpg',
-              '/img/8.jpg',
-              '/img/9.jpg',
-              '/img/10.jpg'
+              '/img/'
             ]
         );
       })
   );
 });
-
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
