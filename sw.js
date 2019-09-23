8ed5c01760cfc2645029910236d4d485ff7373ea
seungho Jeon <urijsh612@gmail.com>
2019-09-23 11:23:55 +0900

    modfied some code

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+var staticCacheName = 'restaurants'
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('restaurants')
+    caches.open(staticCacheName)
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(
@@ -9,6 +11,7 @@ self.addEventListener('install', function(event) {
               '/index.html',
               '/restaurant.html',
               '/css/styles.css',
+              '/js/main.js',
               '/js/dbhelper.js',
               '/data/restaurants.json',
               '/js/restaurant_info.js',
@@ -19,9 +22,23 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
+    caches.match(event.request, {ignoreSearch:true})
       .then(function(response) {
         if (response) {
           return response;
